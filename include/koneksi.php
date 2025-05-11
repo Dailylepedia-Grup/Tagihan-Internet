2de1eea9dcbb7923e28dc5d61779000e5a445328
M Tri Hamdani <53250787+mhdtrihamdanii@users.noreply.github.com>
2025-05-11 22:48:52 +0700

    Add files via upload
    Signed-off-by: M Tri Hamdani <53250787+mhdtrihamdanii@users.noreply.github.com>

---
+++ include/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// $koneksi =  new mysqli("localhost", "root", "", "billing_ibnu");
+
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database_name = "tagihan_internet";
+
+$koneksi = new mysqli($host, $username, $password, $database_name);
